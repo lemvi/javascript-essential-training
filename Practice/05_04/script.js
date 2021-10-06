0e4ea01afe21aa568c48a1046c2e25692bd262db
Viktor Lemanski <viktor.lemanski@tuta.io>
2021-10-06 11:42:37 +0200

    query dom tree and create new dom elements

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,5 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+console.log(document.querySelector("#pack01 .backpack__name").style.color = "grey");
+console.log(document.querySelector("#pack02 .backpack__name").style.color = "green");
--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,50 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+const header = document.querySelector(".siteheader");
+const menu = document.createElement("ul");
+const menuElement1 = document.createElement("li");
+const menuElement2 = document.createElement("li");
+const menuElement3 = document.createElement("li");
+const menuElement4 = document.createElement("li");
+const menuElement5 = document.createElement("li");
+
+menu.insertAdjacentElement("beforeend", menuElement1);
+menu.insertAdjacentElement("beforeend", menuElement2);
+menu.insertAdjacentElement("beforeend", menuElement3);
+menu.insertAdjacentElement("beforeend", menuElement4);
+menu.insertAdjacentElement("beforeend", menuElement5);
+
+menu.style.listStyleType = "none";
+
+menuElement1.insertAdjacentHTML("beforeend", "<a href = '#'>Home</a>");
+menuElement2.insertAdjacentHTML("beforeend", "<a href = '#'>More</a>");
+menuElement3.insertAdjacentHTML("beforeend", "<a href = '#'>Shop</a>");
+menuElement4.insertAdjacentHTML("beforeend", "<a href = '#'>FAQ</a>");
+menuElement5.insertAdjacentHTML("beforeend", "<a href = '#'>About</a>");
+
+menu.classList.add("menu");
+menu.setAttribute("id", "menuBar");
+
+header.append(menu);
+
+console.log(
+  document
+    .querySelectorAll("ul li")
+    .forEach(
+      (item) => (
+        (item.style.display = "inline"), (item.style.margin = "0 10 0 0")
+      )
+    )
+);
+
+console.log(
+  document
+    .querySelectorAll("ul li:first-child")
+    .forEach((item) => (item.style.marginLeft = 0))
+);
+
+console.log(
+  document.querySelectorAll("a").forEach((item) => (item.style.color = "black"))
+);
